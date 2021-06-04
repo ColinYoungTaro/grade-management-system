06fb54a9d850d32230be05ef9064247384da9531
luckyJack <100519@163.com>
2021-06-04 15:57:56 +0800

    web environment config

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        学生成绩管理系统
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-footer'
+    });
+</script>
